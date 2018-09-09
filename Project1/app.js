ebbd42e0e8aea4a3f5000b31e7c1257ee1d10cc0
Wes Easton <wpe19d@gmail.com>
2018-09-09 12:47:40 -0500

    changes

--- Project1/app.js
+++ Project1/app.js
@@ -8,17 +8,21 @@ const WINDOW_COST = 121.17;
 var readLine = require('readline-sync');
 var area = 0;
 
-let purchaseList = {
-    studs: 0,
-    plyWood: 0,
-    door: 1,
-    windows: 3
+let shoppingList = {
+    plyWoodCost: 0,
+    plyWoodAmt: 0,
+    studCost: 0,
+    studAmt: 0,
+    windowCost: 0,
+    windowAmt: 0,
+    doorCost: DOOR_COST,
+    totalCost: 0
 };
 
 //Asks user for the length of the wall.
 while(!length || isNaN(length) || length < 6 || length > 20) {
 
-    var length = readLine.question("What is the length of the shed? (enter a number between 6 and 20): ");    
+    var length = readLine.question("\nWhat is the length of the shed? (enter a number between 6 and 20): ");    
 }
 
 //Asks user for the width of the wall.
@@ -31,11 +35,12 @@ while(!width || isNaN(width) || width < 6 || width > 20 ) {
 while(!numberOfWindows || isNaN(numberOfWindows) || numberOfWindows < 0 || numberOfWindows > 3) {
 
     var numberOfWindows = readLine.question("How many windows will be installed? (enter a number between 0 and 3): ");
+    shoppingList.windowAmt = parseInt(numberOfWindows);
 }
 
-console.log(getNetArea(length, width, numberOfWindows, area));
-console.log(getPlyWoodAmount());
-console.log(getNumberOfStuds(length, width));
+getNetArea(length, width, numberOfWindows, area);
+getPlyWoodAmount();
+getNumberOfStuds(length, width);
 
 //calculates and returns the net area of all the walls.
 function getNetArea(length, width, numberOfWindows) {
@@ -64,9 +69,7 @@ function getDoorArea() {
 
 //calculates and returns the number of plywood sheets needed for the shed.
 function getPlyWoodAmount() {
-    var plyWoodTotal = this.area / (4 * 8);
-    
-    return Math.ceil(plyWoodTotal);
+    shoppingList.plyWoodAmt = Math.ceil(this.area / (4 * 8));
 }
 
 //calculates and returns the number of studs needed for the shed.
@@ -75,5 +78,22 @@ function getNumberOfStuds(length, width) {
     studTotal += (Math.ceil(width / STUD_SPACING) + 1) * 2;
     studTotal += (Math.ceil(length / STUD_LENGTH)) * 4;
     studTotal += (Math.ceil(width / STUD_LENGTH)) * 4;
-    return studTotal;
+    shoppingList.studAmt = studTotal;
 }
+
+shoppingList.plyWoodCost = parseFloat(parseFloat(shoppingList.plyWoodAmt * PLYWOOD_COST).toFixed(2));
+shoppingList.studCost = parseFloat(parseFloat(shoppingList.studAmt * STUD_COST).toFixed(2));
+shoppingList.windowCost = parseFloat(parseFloat(shoppingList.windowAmt * WINDOW_COST).toFixed(2));
+
+//prints out list shopping for each item including the price
+console.log("\nHere's the shopping list for this shed: \n--------------------------------------\n");
+console.log("Windows:        " + numberOfWindows + " - $" + `${shoppingList.windowCost}`);
+console.log("Door:           " + "1" + " - $" + `${DOOR_COST}`);
+console.log(`Plywood Sheets: ${shoppingList.plyWoodAmt} - ${shoppingList.plyWoodCost}`);
+console.log(`Studs:          ${shoppingList.studAmt} - ${shoppingList.studCost}`);
+
+//calculates the total cost
+shoppingList.totalCost = shoppingList.windowCost + DOOR_COST + shoppingList.plyWoodCost + shoppingList.studCost;
+
+//prints out the total cost
+console.log(`Total Cost:          $${shoppingList.totalCost}\n\n`);
